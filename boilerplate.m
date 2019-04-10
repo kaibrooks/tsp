7b22c434d816475ac4562d05898b66ff72010926
Kai Brooks <kaibrooks@gmail.com>
2019-04-10 08:34:42 -0700

    Added format command

--- boilerplate.m
+++ boilerplate.m
@@ -7,13 +7,13 @@
 clc
 close all
 clear all
-
+format
 
 
 
 % Automated feedback script
-% test=scriptname.m;
-% str = '&body= Hi Kai,  %0D%0A  %0D%0A    Your program works well, except for: '; 
-% email=strcat('kbrooks@pdx.edu?subject=[PH322 feedback] %20', test, str ); 
-% url = ['mailto:',email]; 
+% test='filename.m';
+% str = '&body= Hi Kai,  %0D%0A  %0D%0A    Your program works well, except for: ';
+% email=strcat('kbrooks@pdx.edu?subject=[PH322 feedback] %20', test, str );
+% url = ['mailto:',email];
 % web(url)
