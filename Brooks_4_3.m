e4b2982593663143d49c67d3b23ee3214c0341b3
Kai Brooks <kaibrooks@gmail.com>
2019-04-25 15:40:46 -0700

    Boilerplates

---
+++ Brooks_4_3.m
@@ -0,0 +1,20 @@
+% Bifurication II
+% Kai brooks
+% 24 Apr 2019
+% Description
+%
+% Calculate the Feigenbaum constant..
+
+clc
+close all
+clear all
+format
+
+
+
+% Automated feedback script
+% test='filename.m';
+% str = '&body= Hi Kai,  %0D%0A  %0D%0A    Your program works well, except for: ';
+% email=strcat('kbrooks@pdx.edu?subject=[PH322 feedback] %20', test, str );
+% url = ['mailto:',email];
+% web(url)
