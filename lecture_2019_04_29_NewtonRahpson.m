a8fecaa6d405f1fb00f6095a25057017a9abc380
Kai Brooks <kaibrooks@gmail.com>
2019-05-01 12:10:25 -0700

    Post-lecture commit

--- lecture_2019_04_29_NewtonRahpson.m
+++ lecture_2019_04_29_NewtonRahpson.m
@@ -12,16 +12,24 @@ close all
 clear all
 format
 
-f=@(x)x.^2+1;   % Declare function
+f=@(x)x.^2-1;   % Declare function
 d=@(x)2*x;      % Derivative
 x = 1/sqrt(3);
 for n=1:8
-    x=x-f(x)/d(x)
-    y(n) = n;   % for plotting
-    z(n) = x;   % for plotting
+    %x=x-f(x)/d(x);
+    %y(n) = n;   % for plotting
+    %z(n) = x;   % for plotting
 end
 
-plot(y,z);
+xp=Inf;
+
+while abs(x-xp) > 1E-10
+    xp=x;
+    x=x-f(x)/d(x)
+    abs(x-xp) 
+end
+
+%plot(y,z);
 
 % if you dont know if there's a root, plot it
 
