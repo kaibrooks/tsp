6662e19495f99c1e81cfc4e5502b5c4df787ca71
Kai Brooks <kaibrooks@gmail.com>
2019-05-24 22:38:05 -0700

    Updated solutions

--- Brooks_7_1.m
+++ Brooks_7_1.m
@@ -1,55 +1,35 @@
 % Derivatives
 % Kai brooks
 % 16 May 2019
-% Description
-%
-% Plot the forward, backward, and central approximations of the derivative
-% of f(x) = 1000*exp(x/17) at x=3.2, for values of h=0.01 to 5 in steps of
-% 0.05. Compare with the correct value.
+% Plot the forward, backward, and central approximations of the 
+% derivative of ?(?) = 1000 exp (?/17) at ? = 3.2, for values of 
+% ? = 0.01 to 5, in steps of 0.05. Compare with the correct value.
 
 clc
 close all
 clear all
 format
 
-h = 0.01;
-x = linspace(0,10); % Set bounds for plotting
 
-f = @(x) 1000*exp(x/17)
 
-f_x = f(x);       % set f(x) to use in plots
-for (h = 0.01:5:0.05);
-    
-    % forward approx
-    x_f = x(1:end-1);
-    df_f = (f_x(2:end) - f_x(1:end-1))/h;
-    
-    
-    % center approx
-    x_c = x(2:end-1);
-    df_c = (f_x(3:end)-f_x(1:end-2))/(2*h);
-    
-    % backward approx
-    x_b = x(2:end);
-    df_b = (f_x(2:end)-f_x(1:end-1))/h;
-    
-    hold on
-    plot(x_f,df_f); % forward
-    plot(x_b,df_b); % backward
-    plot(x_c,df_c)  % central
-    grid on
-    axis tight
-    legend('Forward','Backward','Central')
-    
+f=@(x)1000*exp(x/17);
+X=3.2;
+h=[0.01:0.05:5];
+y=zeros(4,length(h));
+
+for k=1:length(h)
+    y(1,k)=((1000/17)*exp(X/17));
+    y(2,k)=(f(X+h(k))-f(X))/h(k);
+    y(3,k)=(f(X+(h(k)./2))-f(X-(h(k)./2)))/h(k);
+    y(4,k)=(f(X)-f(X-h(k)))/h(k);
 end
 
-% exact value
-a = f(3.2)
-fprintf('Compare to exact value: %f\n', a)
+plot(h,y)
+legend('Actual Value','Forward Derivitive','Central Derivitive','Backward Derivitive')
 
 % Automated feedback script
 test='Brooks_7_1.m';
 str = '&body= Hi Kai,  %0D%0A  %0D%0A    Your program works well, except for: ';
 email=strcat('kbrooks@pdx.edu?subject=[PH322 feedback] %20', test, str );
 url = ['mailto:',email];
-% web(url)
+web(url)
--- Brooks_7_2.m
+++ Brooks_7_2.m
@@ -3,10 +3,9 @@
 % 16 May 2019
 % Description
 %
-% Integrate f(x)dx from 0 to pi using trapezoidal, simpsons, and simpsons
-% 3/8 rules. Use 3N intervals (3N+1 points) where N = 16,32,...1024.
-% Compare accuracy of the various methods with equal number of function
-% evaluations.
+%Integrate sin(x) from 0 to pi using trapezoidal, Simpsonís, and Simpsonís 3/8th rules, using your code.  
+%The answer is 2.  Compare accuracy of the various methods with equal number of function evaluations, 
+%so you could plot, for the various rules, the result minus 2 vs the number of evaluations. 
 
 clc
 close all
@@ -14,10 +13,36 @@ clear all
 format
 
 
+N = 2.^[4:10];
+f = @(x) sin(x);
+for i=1:numel(N)
+x = linspace(0,pi,3*N(i)+1);
+y = f(x);
+% simpsons_3_8 = (3*h./8)*(f_0+3*f_1+3*f_2+f_3); 
+h=pi/(3*N(i));
+int{1}(i)=trapz(x,y);
+int{2}(i)= (h/3)*(y(1)+2*sum(y(3:2:end-2))+4*sum(y(2:2:end))+y(end));
+int{3}(i)= (3*h/8)*(y(1)+3*sum(y(2:3:end))+3*sum(y(3:3:end))+2*sum(y(4:3:end-1))+y(end));
+end
+
+for k = 1:3
+subplot(3,1,k);
+loglog(3*N,int{k}-2);
+xlim([3*N(1) 3*N(end)]);
+xlabel('N');
+ylabel('error');
+grid on
+end
+subplot(3,1,1);
+title('Trapezoidal Rule Errors');
+subplot(3,1,2);
+title('Simpsons Rule Errors');
+subplot(3,1,3);
+title('Simpsons 3/8 Rule Errors');
 
 % Automated feedback script
-% test='Brooks_7_2.m';
-% str = '&body= Hi Kai,  %0D%0A  %0D%0A    Your program works well, except for: ';
-% email=strcat('kbrooks@pdx.edu?subject=[PH322 feedback] %20', test, str );
-% url = ['mailto:',email];
-% web(url)
+test='Brooks_7_2.m';
+str = '&body= Hi Kai,  %0D%0A  %0D%0A    Your program works well, except for: ';
+email=strcat('kbrooks@pdx.edu?subject=[PH322 feedback] %20', test, str );
+url = ['mailto:',email];
+web(url)
