9b4205f841933229cc84a95a8ac85a36a681db18
Kai Brooks <kaibrooks@gmail.com>
2019-05-24 22:38:24 -0700

    WIP

--- lecture_2019_05_20.m
+++ lecture_2019_05_20.m
@@ -1,64 +1,65 @@
-% Random II
+% Birthday checker
 % Kai brooks
 % 20 May 2019
 % Description
 %
-% Find the odds of two people sharing the same birthday in a set of n
-% people
+% Find odds of two people sharing the same birthday in a set of n people
 
 clc
 close all
 clear all
 format
 
-setSize = 30;  % number of integers to generate
-epochs = 1000;
+setSize = 50;  % number of integers to generate
+epochs = 100;
+iterations = 100; % how far to plot
 
 loops = 0;  % timing calculation
 found = 0; % break loop
 totalMatches = 0;
 
-
-
-for int = 1:epochs
-    x = randi([1 365],[setSize 1]) % generate k random integers
-    
-    for n=1:setSize-1
-        for m=1+n:setSize-1
+for i = 1:iterations
+    for int= 1:epochs
+        x = randi([1 365],[setSize 1]); % generate k random integers
+        
+        for n=1:setSize-1
+            for m=1+n:setSize-1
+                
+                if x(n) == x(m)
+                    %fprintf('found on day %i \n',x(n))
+                    found = 1;
+                end
+                
+                loops = loops+1;
+                
+                if found == 1
+                    break
+                end
+            end
             
             if x(n) == x(m)
                 %fprintf('found on day %i \n',x(n))
                 found = 1;
             end
             
-            
-            loops = loops+1;
-            
             if found == 1
                 break
             end
         end
         
-        if x(n) == x(m)
-            %fprintf('found on day %i \n',x(n))
-            found = 1;
+        % add total if found
+        if found==1
+            totalMatches = totalMatches+1;
         end
         
-        if found == 1
-            break
-        end
+        found = 0;
+        
     end
     
-    % add total if found
-    if found==1
-        totalMatches = totalMatches+1;
-    end
+    prob(i,1) = totalMatches/epochs;
+    prob(i,2) = i;
     
-    found = 0;
-
 end
-
-totalMatches/epochs
 
 
 % Automated feedback script
